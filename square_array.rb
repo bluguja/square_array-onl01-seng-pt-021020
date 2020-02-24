4e03db01df42f3e6e444fda525fffada2e9a0e31
Bala Luguja <bluguja@gmail.com>
2020-02-24 20:07:08 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,10 @@
-def square_array(array)
-  # your code here
+numbers = [1,2,3]
+new_numbers = {9,10,16,25]
+
+def square_array(numbers)
+  numbers.each do |number|
+    puts number*number
+  end
 end
+squre_array(numbers)
+squre_array(new_numbers)
